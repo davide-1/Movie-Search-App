99a97657fb52196e67c2575ab7d925e9358ce9e3
Davide <davidelaplaca92@gmail.com>
2025-09-20 18:20:31 -0700

    improve MovieSearch UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate  } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage"; 
 import MovieDetails from "./components/MovieDetails";
 import FavoritesPage from "./components/FavoritesPage";
+import SearchPage from "./components/SearchPageProps";
 
 
 function App() {
+  const navigate = useNavigate();
+
+
+  const handleSearch = (query: string) => {
+    const q = query.trim();
+    if (!q) return;
+    navigate(`/search?q=${encodeURIComponent(q)}`);
+  };
+
+
   return (
     <div style={{ backgroundColor: "#121212", minHeight: "100vh", color: "#fff", margin: 0, padding: 0, overflowX: "hidden", }}>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage onSearch={handleSearch} />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/search" element={<SearchPage onSearch={handleSearch} />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </div>
